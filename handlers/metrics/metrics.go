f3d43e79075de5f3c11f7af2d6fefa2c23e0c791
Christian Mague <cmague@konghq.com>
2018-04-17 11:29:53 -0700

    more discovery

--- handlers/metrics/metrics.go
+++ handlers/metrics/metrics.go
@@ -3,12 +3,13 @@ package metrics
 import (
 	"bytes"
 	//	"encoding/json"
-	//	"errors"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	//	"io/ioutil"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 	"regexp"
 	"github.com/hashicorp/consul/api"
 )
@@ -24,10 +25,10 @@ func handleError(c *gin.Context, errorString error) {
 func Metrics(c *gin.Context) {
 	var statusUrl string
 
-	if os.Getenv("CONSUL_URL") == "" {
-		statusUrl = "http://consul:8500/"
+	if os.Getenv("CONSUL_ADDRESS") == "" {
+		statusUrl = "consul:8500"
 	} else {
-		statusUrl = os.Getenv("CONSUL_URL")
+		statusUrl = os.Getenv("CONSUL_ADDRESS")
 	}
 
 	perfdata, err := ScrapeMetrics(c, statusUrl)
@@ -44,10 +45,45 @@ func Metrics(c *gin.Context) {
 func ScrapeMetrics(c *gin.Context, url string) (bytes.Buffer, error) {
 	var metricString bytes.Buffer
 
-	consulConf := api.Config{Address: "consul-infra-aws-us-west-1.kongcloud.io:8500"}
+	nodes, err := DiscoverNodes(c, url)
+	if err != nil {
+		return metricString, err
+	}
+	for _, n := range nodes {
+		statusClient := http.Client{
+						Timeout: time.Second * 3,
+		}
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%s/", n), nil)
+		if err != nil {
+						return metricString, err
+		}
+		res, getErr := statusClient.Do(req)
+		if getErr != nil {
+						handleError(c, getErr)
+						return metricString, getErr
+		}
+		if res.StatusCode != 200 {
+						c.JSON(500, gin.H{"message": "Call to the api endpoint failed", "http_status": res.StatusCode})
+						return metricString, errors.New("HTTP Response: was not 200")
+		}
+		body, readErr := ioutil.ReadAll(res.Body)
+		if readErr != nil {
+						return metricString, readErr
+		}
+		fmt.Println(body)
+	}
+
+	metricString.WriteString(fmt.Sprintf("# HELP Can we fetch the catalog from consul\n# TYPE consul_catalog_available gauge\nconsul_catalog_available %d\n", 1))
+	return metricString, nil
+}
+
+func DiscoverNodes(c *gin.Context, url string) ([]string, error) {
+	var nodeList []string
+
+	consulConf := api.Config{Address: url}
 	client, err := api.NewClient(&consulConf)
 	if err != nil {
-		panic(err)
+		return nodeList, err
 	}
 
 	catalog := client.Catalog()
@@ -55,18 +91,14 @@ func ScrapeMetrics(c *gin.Context, url string) (bytes.Buffer, error) {
 	//res, _, _ := catalog.Services(&srvs)
 	res, _, err := catalog.Service("vault", "", &srvs)
 	if err != nil {
-		panic(err)
+		return nodeList, err
 	}
 
 	r, _ := regexp.Compile(`^vault-.*$`)
-	for _,s := range res {
+	for _, s := range res {
 			if r.MatchString(s.Node) == true {
-				fmt.Println(s.Node)
-				fmt.Println(s.ServicePort)
+				nodeList = append(nodeList, fmt.Sprintf("%s:%d", s.Node, s.ServicePort))
 			}
 	}
-
-
-	metricString.WriteString(fmt.Sprintf("# HELP Can we fetch the catalog from consul\n# TYPE consul_catalog_available gauge\nconsul_catalog_available %d\n", 1))
-	return metricString, nil
+	return nodeList, nil
 }
